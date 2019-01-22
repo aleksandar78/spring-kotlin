233d5bacb917659479e4c4be7850fdf22f7a96b2
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2019-01-22 22:01:28 +0100

    Add junit5 and e2e test case

--- src/test/kotlin/org/aroundbit/springkotlin/SpringKotlinApplicationTests.kt
+++ src/test/kotlin/org/aroundbit/springkotlin/SpringKotlinApplicationTests.kt
@@ -1,16 +1,28 @@
 package org.aroundbit.springkotlin
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
-class SpringKotlinApplicationTests {
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringKotlinApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
 
 	@Test
 	fun contextLoads() {
+		val ent = restTemplate.getForEntity<String>("/api/v1/currentdate")
+		assertThat(ent.statusCode, equalTo(HttpStatus.OK))
+		val body = ent.body
+		assertThat(body.isNullOrEmpty(), equalTo(false))
+		LoggerFactory.getLogger(this::class.java).info("Test ended correctly: " + body)
 	}
 
 }
